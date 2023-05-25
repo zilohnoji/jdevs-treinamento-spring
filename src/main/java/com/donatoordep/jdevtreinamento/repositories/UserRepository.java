c5751c0231eda6cf1971aa9b921286b31f23524f
Pedro Donato <emagdonatdonatoordep@gmail.com>
2023-05-25 16:19:56 -0300

    Page web added

--- src/main/java/com/donatoordep/jdevtreinamento/repositories/UserRepository.java
+++ src/main/java/com/donatoordep/jdevtreinamento/repositories/UserRepository.java
@@ -4,11 +4,9 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import com.donatoordep.jdevtreinamento.entities.UserEntity;
 
-@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
 	@Query(value = "SELECT u FROM UserEntity u WHERE LOWER(u.name) LIKE %:name%")
